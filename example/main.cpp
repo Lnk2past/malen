16a999ffc68d0389fb72ff7e682f10afc196631e
Lnk2past <Lnk2past@gmail.com>
2019-11-14 15:57:07 -0500

    Add kwargs back in

--- example/main.cpp
+++ example/main.cpp
@@ -24,7 +24,7 @@ int main()
             y_data.push_back(dis(eng));
             c_data.push_back(dis(eng));
         }
-        pyvis.plot_color(figure, "scatter", x_data, y_data, c_data);
+        pyvis.plot_color(figure, "scatter", x_data, y_data, c_data, "radius", 2);
         pyvis.generate_html(figure, "test.html");
     }
     {
