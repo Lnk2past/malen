c9f07cc5f2b87622498f703069e1ea8dd1565eac
Lnk2past <Lnk2past@gmail.com>
2019-12-19 14:24:32 -0500

    Strip out bokeh specific things, lazily load methods

--- include/malen/malen.h
+++ include/malen/malen.h
@@ -9,6 +9,7 @@
 
 #include <stdexcept>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "malen/py_conversions.h"
@@ -19,7 +20,7 @@ namespace malen
 class Malen
 {
 public:
-    Malen(const std::vector<std::string> &additional_paths = std::vector<std::string>())
+    Malen(const std::string &module_name, const std::vector<std::string> &additional_paths = std::vector<std::string>())
     {
         Py_Initialize();
         add_to_path(".");
@@ -27,22 +28,12 @@ public:
         {
             add_to_path(path);
         }
-        py_module = PyImport_ImportModule("malen");
+        py_module = PyImport_ImportModule(module_name.c_str());
         if (!py_module)
         {
             PyErr_PrintEx(1);
-            throw std::runtime_error("There were problems loading the malen module.");
+            throw std::runtime_error("There were problems loading the " + module_name + " module.");
         }
-
-        make_new_figure_handle = load_method("make_new_figure");
-        plot_handle = load_method("plot");
-        image_handle = load_method("image");
-        slider_handle = load_method("slider");
-        ticker_handle = load_method("ticker");
-        color_mapper_handle = load_method("color_mapper");
-        color_bar_handle = load_method("color_bar");
-        layout_handle = load_method("layout");
-        generate_html_handle = load_method("generate_html");
     }
 
     ~Malen()
@@ -50,65 +41,20 @@ public:
         Py_Finalize();
     }
 
-    template <typename... KW>
-    PyObject* make_new_figure(const std::string &title, KW... kw)
+    template<typename... V>
+    PyObject* operator()(const std::string &handle_name, PyObject *kwargs, V... v)
     {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        return py_call_object(make_new_figure_handle, __FUNCTION__ , kwargs, title);
-    }
-
-    template <template<typename...> class C1, template<typename...> class C2, typename T1, typename T2, typename... KW>
-    PyObject* plot(PyObject* figure, const std::string &plot_type, const C1<T1> &datax, const C2<T2> &datay, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        return py_call_object(plot_handle, __FUNCTION__, kwargs, figure, plot_type, datax, datay);
-    }
-
-    template <template<typename...> class C, typename T, typename... KW>
-    PyObject* image(PyObject* figure, const C<C<T>> &image, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        return py_call_object(image_handle, __FUNCTION__, kwargs, figure, image);
-    }
-
-    template <typename... KW>
-    PyObject* slider(PyObject* renderer, const std::string &title, const std::size_t start, const std::size_t end, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        return py_call_object(slider_handle, __FUNCTION__, kwargs, renderer, title, start, end);
-    }
-
-    template <typename... KW>
-    PyObject* ticker(const std::string &ticker_type, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        return py_call_object(ticker_handle, __FUNCTION__, kwargs, ticker_type);
-    }
-
-    template <typename... KW>
-    PyObject* color_mapper(const std::string &mapper_type, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        return py_call_object(color_mapper_handle, __FUNCTION__, kwargs, mapper_type);
-    }
-
-    template <typename... KW>
-    void color_bar(PyObject *figure, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        py_call_object(color_bar_handle, __FUNCTION__, kwargs, figure);
-    }
-
-    PyObject* layout(PyObject* obj1, PyObject *obj2)
-    {
-        return py_call_object(layout_handle, __FUNCTION__, nullptr, obj1, obj2);
-    }
-
-    template <typename... KW>
-    void generate_html(PyObject *figure, const std::string &filename, KW... kw)
-    {
-        auto kwargs = build_kwargs(nullptr, kw...);
-        py_call_object(generate_html_handle, __FUNCTION__, kwargs, figure, filename);
+        PyObject *args = set_arguments(nullptr, 0, v...);
+        PyObject *pyRetval = PyObject_Call(get_python_method(handle_name), args, kwargs);
+        if (!pyRetval)
+        {
+            PyErr_PrintEx(1);
+            throw std::runtime_error("Failed to invoke the " +  handle_name + " handle!");
+        }
+        Py_INCREF(pyRetval);
+        // Py_DECREF(args);
+        // TODO: Determine issue with this Py_DECREF...
+        return pyRetval;
     }
 
 private:
@@ -120,14 +66,18 @@ private:
         PyList_Append(PySys_GetObject("path"), convert_to_python(path));
     }
 
-    inline PyObject* load_method(const std::string &method_name)
+    inline PyObject* get_python_method(const std::string &method_name)
     {
-        PyObject *handle = PyObject_GetAttrString(py_module, method_name.c_str());
-        if (!handle)
+        if (py_methods.find(method_name) == std::end(py_methods))
         {
-            throw std::runtime_error("There were problems loading the malen." + method_name + " method.");
+            PyObject *handle = PyObject_GetAttrString(py_module, method_name.c_str());
+            if (!handle)
+            {
+                throw std::runtime_error("There were problems loading the " + method_name + " method.");
+            }
+            py_methods[method_name] = handle;
         }
-        return handle;
+        return py_methods[method_name];
     }
 
     inline PyObject* make_py_tuple(std::size_t size)
@@ -181,31 +131,7 @@ private:
         return kwargs;
     }
 
-    template<typename... V>
-    inline PyObject* py_call_object(PyObject *handle, const std::string &handle_name, PyObject *kwargs, V... v)
-    {
-        PyObject *args = set_arguments(nullptr, 0, v...);
-        PyObject *pyRetval = PyObject_Call(handle, args, kwargs);
-        if (!pyRetval)
-        {
-            PyErr_PrintEx(1);
-            throw std::runtime_error("Failed to invoke the " +  handle_name + " handle!");
-        }
-        Py_INCREF(pyRetval);
-        // Py_DECREF(args);
-        // TODO: Determine issue with this Py_DECREF...
-        return pyRetval;
-    }
-
     PyObject *py_module = nullptr;
-    PyObject *make_new_figure_handle = nullptr;
-    PyObject *plot_handle = nullptr;
-    PyObject *image_handle = nullptr;
-    PyObject *slider_handle = nullptr;
-    PyObject *ticker_handle = nullptr;
-    PyObject *color_mapper_handle = nullptr;
-    PyObject *color_bar_handle = nullptr;
-    PyObject *layout_handle = nullptr;
-    PyObject *generate_html_handle = nullptr;
+    std::unordered_map<std::string, PyObject*> py_methods = {};
 };
 }
